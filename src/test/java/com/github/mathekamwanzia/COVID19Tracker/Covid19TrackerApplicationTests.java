177be16ac5499aafcaddcc2417be7e5494276f8f
mathekamwanzia <mwanziapeter489@gmail.com>
2020-04-04 12:38:06 +0300

    Tracks the new cases and total deaths

---
+++ src/test/java/com/github/mathekamwanzia/COVID19Tracker/Covid19TrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.mathekamwanzia.COVID19Tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Covid19TrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
